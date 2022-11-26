3d2c934ba1e9d6027521dea6e5df2993f702073c
Nikita Pletnev <betpezeh1@yahoo.com>
2022-11-26 14:42:51 +0500

    add new examples homeWork

---
+++ example001.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+day_number = int(input('Введите цифру обозночающую день недели'))
+if day_number == 1:
+    print('no')
+if day_number == 2:
+    print('no')
+if day_number == 3:
+    print('no')
+if day_number == 4:
+    print('no')
+if day_number == 5:
+    print('no')
+if day_number == 6:
+    print('yes')
+if day_number == 7:
+    print('yes')
